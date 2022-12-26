aa791e7c3f23cc00f55da8c81613f6c8771bb1a4
Arizona Hanson <arizona@starlig.ht>
2022-12-26 16:35:42 -0500

    stream: redo *one-of, *one-or-more

--- src/core/stream.scm
+++ src/core/stream.scm
@@ -5,23 +5,25 @@
     %stream?
     %lseq
     %pos
-    port->stream%
+    generator->stream%
     result%
     %result
     %result?
     %value
-    %input
+    %rest
     *return
     *fail
     *identity
     *bind
     *one-of
-    *null
+    *nothing
     *maybe
-    *each-of
+    *is
+    *not
+    *end
+    *list
     *zero-or-more
-    *one-or-more
-    *is)
+    *one-or-more)
 
   (import
     r5rs
@@ -31,99 +33,88 @@
     srfi-127
     srfi-158)
 
-  ; 𝒑 -> 𝒍
-  (define (port->lseq port)
-    (generator->lseq
-      (make-coroutine-generator
-        (lambda (yield)
-          (let loop
-            ((ch (read-char port)))
-            (cond
-              ((eof-object? ch))
-              (else
-                (yield ch)
-                (loop (read-char port)))))))))
-
-  ; 𝒍 𝑛 -> 𝜸ₙ
   (define-record-type stream%
     (%stream lseq pos)
     %stream?
     (lseq %lseq)
     (pos %pos))
 
-  ; 𝒑 -> 𝜸₀
-  (define (port->stream% port)
-    (%stream (port->lseq port) 0))
-
-  ; 𝑣 𝜸ₙ -> 𝑣.𝜸ₙ
   (define-record-type result%
-    (%result value input)
+    (%result value stream)
     %result?
     (value %value)
-    (input %input))
+    (stream %rest))
 
-  ; 𝑣 -> 𝜸ₙ -> 𝑣.𝜸ₙ
+  (define (generator->stream% gen)
+    (%stream (generator->lseq gen) 0))
+
+  (define (%empty? stream)
+    (null? (%lseq stream)))
+
+  (define (port->generator port)
+    (make-coroutine-generator
+      (lambda (yield)
+        (let loop
+          ((char (read-char port)))
+          (cond
+            ((eof-object? char))
+            (else
+              (yield char)
+              (loop (read-char port))))))))
+
+  ; 𝑣 -> 𝜸ₓ -> 𝑣.𝜸ₓ
+  ; 𝑣 => 𝑣
   (define ((*return value) input)
     (%result value input))
 
-  ; -> 𝜸ₙ -> ⊥
-  (define ((*fail) input) #f)
+  ;  -> 𝜸ₓ -> ⊥
+  ;  => ⊥
+  (define ((*fail) input)
+    #f)
 
-  ; -> 𝜸ₙ -> 𝑥ₙ.𝜸ₙ₊ | ⊥
+  ;  -> 𝜸ₓ -> 𝑥.𝜸₊ | ⊥
+  ;  => 𝑥 | ⊥
   (define ((*identity) input)
-    (cond
-      ((null? (%lseq input)) #f)
-      (else
-        (%result
-          (lseq-car (%lseq input))
-          (%stream
-            (lseq-cdr (%lseq input))
-            (+ 1 (%pos input)))))))
+    (if (%empty? input) #f
+      (%result
+        (lseq-car (%lseq input))
+        (%stream
+          (lseq-cdr (%lseq input))
+          (+ 1 (%pos input))))))
 
-  ; >>=
-  ; 𝑝 𝑘' => 𝑣ₚ -> 𝑣ₖ | ⊥
+  ; 𝑟 𝑘' => 𝑘'𝑣ʳ -> 𝑣ᵏ | ⊥
   (define ((*bind parser combinator) input)
     (and-let*
       ((result (parser input)))
-      ((combinator (%value result)) (%input result))))
+      ((combinator (%value result)) (%rest result))))
 
-  ; <|>
-  ; 𝑝 𝑟* => 𝑣ₚ | 𝑣ᵣ... | ⊥
-  (define ((*one-of parser . parsers) input)
-    (let
-      ((result (parser input)))
-      (cond
-        ((or result (null? parsers)) result)
-        (else
-          ((apply *one-of parsers) input)))))
+  ; 𝑟 𝑠.. => 𝑣ʳ | 𝑣ˢ.. | ⊥
+  (define ((*one-of . parsers) input)
+    (and
+      (not (null? parsers))
+      (or
+        ((car parsers) input)
+        ((apply *one-of (cdr parsers)) input))))
 
-  ; => ∅
-  (define (*null)
+  ;  => ∅
+  (define (*nothing)
     (*return '()))
 
-  (define (*each-of . parsers)
-    (cond
-      ((null? parsers) (*null))
-      (else
-        (*bind
-          (car parsers)
-          (lambda (value)
-            (*bind
-              (apply *each-of (cdr parsers))
-              (lambda (value')
-                (*return (cons value value')))))))))
-
-  ; 𝑝 => 𝑣ₚ | ∅
+  ; 𝑟 => 𝑣ʳ | ∅
   (define (*maybe parser)
-    (*one-of parser (*null)))
+    (*one-of parser (*nothing)))
 
-  (define (*one-or-more parser)
-    (*each-of parser (*zero-or-more parser)))
+  ; 𝑟 => 𝑣ʳ ∴ ⊥ | ∅
+  (define ((*not parser) input)
+    (and
+      (not (parser input))
+      ((*nothing) input)))
 
-  (define (*zero-or-more parser)
-    (*maybe
-      (*one-or-more parser)))
+  ;  => 𝑥 ∴ ⊥ | ∅
+  (define (*end)
+    (*not (*identity)))
 
+  ; 𝑒 => 𝑒𝑥 ∴ 𝑥 | ⊥
   (define (*is predicate . args)
     (*bind
       (*identity)
@@ -131,5 +122,30 @@
         (if (apply predicate value args)
           (*return value)
           (*fail)))))
+
+  ; 𝑟 𝑠.. => [𝑣ʳ, 𝑣ˢ..] | ⊥
+  (define (*list . parsers)
+    (if (null? parsers) (*nothing)
+      (*bind
+        (car parsers)
+        (lambda (value)
+          (*bind
+            (apply *list (cdr parsers))
+            (lambda (value')
+              (*return (cons value value'))))))))
+
+  ; 𝑟 => [𝑣ʳ, <𝑣ʳ..>] | ⊥
+  (define (*one-or-more parser)
+    (*bind
+      parser
+      (lambda (value)
+        (*list
+          (*return value)
+          (*zero-or-more parser)))))
+
+  ; 𝑟 => [𝑣ʳ, <𝑣ʳ..>] | ∅
+  (define (*zero-or-more parser)
+    (*maybe
+      (*one-or-more parser)))
 
 )
